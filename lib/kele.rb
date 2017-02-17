cd5b1fe6b9a51fce563f60128a6cf87eabcacc01
Victoria Fung <victoriahkfung@gmail.com>
2017-02-16 23:36:20 -0800

    get_mentory_availability strip error

--- lib/kele.rb
+++ lib/kele.rb
@@ -7,14 +7,18 @@ class Kele
 
   def initialize(username, password)
     response = self.class.post('/sessions', body: { "username": username, "password": password } )
-    puts response.code
     raise "invalid email/password" if response.code == 404
     @auth_token = response["auth_token"]
   end
 
   def get_me
     response = self.class.get('/users/me', headers: { "authorization" => @auth_token } )
-    JSON.parse(response.body)
+    @user = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token } )
+    @mentor_availability = JSON.parse(response.body)
   end
 
   # private
