c8dffc7922d3056b57b5aa4e1d79f0790203608a
Victoria Fung <victoriahkfung@gmail.com>
2017-02-16 23:15:38 -0800

    defined get_me with no method error

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,18 +1,24 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
-  # base_uri "https://www.bloc.io/api/v1/"
+  base_uri "https://www.bloc.io/api/v1"
 
   def initialize(username, password)
-    response = self.class.post(base_api_endpoint("sessions"), body: { "username": username, "password": password } )
+    response = self.class.post('/sessions', body: { "username": username, "password": password } )
     puts response.code
     raise "invalid email/password" if response.code == 404
     @auth_token = response["auth_token"]
   end
 
-  private
-  def base_api_endpoint(session)
-    "https://www.bloc.io/api/v1/#{session}"
+  def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token } )
+    JSON.parse(response.body)
   end
+
+  # private
+  # def base_api_endpoint(session)
+  #   "https://www.bloc.io/api/v1/#{session}"
+  # end
 end
